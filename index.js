ed1565299215e8487fabdb5b9996898fbbc5a74b
Cloud Platform <cloudxplatform@gmail.com>
2018-09-02 19:08:51 +0200

    Add queries

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ require('@google/cloud-debug');
 
 exports.helloworld=function helloworld(req,res){
     let name=req.query.name||'Roma';
+    let mood=req.query.mood='Happy';
+    let weather=false;
+    if(!weather){
+        console.error('Too bad for me!');
+    }
+    console.log('Great to have my first cloud function');
     res.status(200).send('Hi from ${name}.');
 }
